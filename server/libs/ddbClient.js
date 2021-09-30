e27c5dc3649f35ae2e93b50478d09a3d0caa0e1e
esteban miño <minoesteban1@gmail.com>
2021-09-30 20:35:17 +0200

    updates

--- server/libs/ddbClient.js
+++ server/libs/ddbClient.js
@@ -12,7 +12,7 @@ INPUTS:
 // Create service client module using ES6 syntax.
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 // Set the AWS Region.
-const REGION = "us-east-1"; //e.g. "us-east-1"
+const REGION = "eu-north-1";
 // Create an Amazon DynamoDB service client object.
 const ddbClient = new DynamoDBClient({ region: REGION });
 module.exports = { ddbClient };
